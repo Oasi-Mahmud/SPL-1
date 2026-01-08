8f858cede4a3753b9cd25e6eae9f113b61b8e3e7
Oasi-Mahmud <oasimahmud41@gmail.com>
2026-01-08 22:52:23 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -20,3 +20,57 @@ void analyze(Token *t, int n) {
     int insideMethod = 0;
 
     printf("\nDetected Classes:\n");
+
+      for (int i = 0; i < n - 1; i++) {
+        if (t[i].type == TOK_KEYWORD &&
+            strcmp(t[i].lexeme, "class") == 0 &&
+            t[i+1].type == TOK_IDENTIFIER) {
+
+            printf("  Class: %s (line %d)\n",
+                   t[i+1].lexeme, t[i].line);
+            classCount++;
+        }
+    }
+
+    printf("\nDetected Methods:\n");
+
+    for (int i = 0; i < n - 2; i++) {
+        if ((t[i].type == TOK_KEYWORD && is_type_keyword(t[i].lexeme)) &&
+            t[i+1].type == TOK_IDENTIFIER &&
+            strcmp(t[i+2].lexeme, "(") == 0) {
+
+            printf("  Method: %s (line %d)\n",
+                   t[i+1].lexeme, t[i].line);
+            methodCount++;
+        }
+
+        if (t[i].type == TOK_KEYWORD &&
+            strcmp(t[i].lexeme,"void")==0 &&
+            t[i+1].type == TOK_IDENTIFIER &&
+            strcmp(t[i+2].lexeme,"(")==0) {
+
+            printf("  Method: %s (line %d)\n",
+                   t[i+1].lexeme, t[i].line);
+            methodCount++;
+        }
+    }
+
+    printf("\nDetected Loops:\n");
+
+    for (int i = 0; i < n; i++) {
+        if (t[i].type == TOK_KEYWORD) {
+            if (strcmp(t[i].lexeme,"for")==0) {
+                printf("  for-loop (line %d)\n", t[i].line);
+                forCount++;
+            }
+            if (strcmp(t[i].lexeme,"while")==0) {
+                printf("  while-loop (line %d)\n", t[i].line);
+                whileCount++;
+            }
+            if (strcmp(t[i].lexeme,"do")==0) {
+                printf("  do-while-loop (line %d)\n", t[i].line);
+                doCount++;
+            }
+        }
+    }
+
