8d89f5dab88aa820eb4b0ea02f19bbf7ceef122b
Oasi-Mahmud <oasimahmud41@gmail.com>
2026-02-03 11:13:47 +0600

    Update summary.c

--- src/summary.c
+++ src/summary.c
@@ -1,1 +1,13 @@
+#include "summary.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+static void to_lower_copy(const char *in, char *out, size_t outsz) {
+    size_t j = 0;
+    for (size_t i = 0; in[i] && j + 1 < outsz; i++) {
+        out[j++] = (char)tolower((unsigned char)in[i]);
+    }
+    out[j] = '\0';
+}
 
