ed29a9c36d218a17b0d7108d5f1bc80bcd53e274
Oasi-Mahmud <oasimahmud41@gmail.com>
2026-02-26 03:03:44 +0600

    Add keyword and symbol checking functions

--- src/bug_rules.c
+++ src/bug_rules.c
@@ -1,1 +1,23 @@
+#include "bug_rules.h"
+#include <stdio.h>
+#include <string.h>
 
+
+static int is_type_keyword_local(const char *s) {
+    return strcmp(s,"int")==0 || strcmp(s,"double")==0 ||
+           strcmp(s,"float")==0 || strcmp(s,"char")==0 ||
+           strcmp(s,"boolean")==0 || strcmp(s,"long")==0 ||
+           strcmp(s,"short")==0;
+}
+
+static int is_symbol(Token *t, int i, const char *sym) {
+    return t[i].type == TOK_SYMBOL && strcmp(t[i].lexeme, sym) == 0;
+}
+
+static int is_keyword(Token *t, int i, const char *kw) {
+    return t[i].type == TOK_KEYWORD && strcmp(t[i].lexeme, kw) == 0;
+}
+
+static int is_control_kw(const char *s) {
+    return strcmp(s,"if")==0 || strcmp(s,"for")==0 || strcmp(s,"while")==0;
+}
