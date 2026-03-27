2c67ada260cf29009909797ef89b846fac72155c
Oasi-Mahmud <oasimahmud41@gmail.com>
2026-03-27 19:12:08 +0600

    Define token types and structures in token.h

--- src/token.h
+++ src/token.h
@@ -1,1 +1,23 @@
+#ifndef TOKEN_H
+#define TOKEN_H
 
+typedef enum {
+    TOK_IDENTIFIER,
+    TOK_KEYWORD,
+    TOK_NUMBER,
+    TOK_STRING,
+    TOK_SYMBOL,
+    TOK_EOF
+} JTokenType;
+
+typedef struct {
+    JTokenType type;
+    char *lexeme;
+    int line;
+    int column;
+} Token;
+
+Token *lex_file(const char *filename, int *out_count);
+void free_tokens(Token *tokens, int count);
+
+#endif
