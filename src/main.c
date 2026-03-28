e3cb24471816fef48ca073230a4ccc85861b33cc
Oasi-Mahmud <oasimahmud41@gmail.com>
2026-03-29 01:57:57 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -16,11 +16,11 @@ static int is_type_keyword(const char *s) {
 
 void analyze(Token *t, int n) {
 
-int classCount=0, methodCount =0;
-    int forCount =0, whileCount= 0, doCount = 0;
-    int globalVar =0, localVar= 0;
-    int LOC =0;
-    int braceDepth= 0;
+int classCount=0,methodCount =0;
+    int forCount =0, hileCount= 0, doCount = 0;
+    int globalVar =0,localVar= 0;
+    int LOC=0;
+    int braceDepth=0;
     int insideMethod=0;
 
     char methodNames[100][64];
@@ -31,13 +31,13 @@ int classCount=0, methodCount =0;
     printf("\nDetected Classes:\n");
 
       for (int i=0;i< n- 1;i++) {
-        if (t[i].type == TOK_KEYWORD &&
-            strcmp(t[i].lexeme, "class") ==0 &&
-            t[i+1].type == TOK_IDENTIFIER) {
+        if (t[i].type ==TOK_KEYWORD &&
+            strcmp(t[i].lexeme,"class") ==0 &&
+            t[i+1].type ==TOK_IDENTIFIER) {
 
             printf("  Class: %s (line %d)\n",
                    t[i+1].lexeme,t[i].line);
-             if (classCount < 100) {
+             if(classCount<100){
                 strcpy(classNames[classCount],t[i+1].lexeme);
                 classLines[classCount] = t[i].line;
             }
@@ -50,25 +50,25 @@ int classCount=0, methodCount =0;
     for (int i=0; i<n-2;i++) {
         if ((t[i].type==TOK_KEYWORD && is_type_keyword(t[i].lexeme)) &&
             t[i+1].type ==TOK_IDENTIFIER &&
-            strcmp(t[i+2].lexeme, "(")==0) {
+            strcmp(t[i+2].lexeme, "(")==0){
 
             printf("  Method: %s (line %d)\n",
                    t[i+1].lexeme,t[i].line);
-             if (methodCount<100) {
+             if(methodCount<100){
                 strcpy(methodNames[methodCount], t[i+1].lexeme);
                 methodLines[methodCount]=t[i].line;
             }
             methodCount++;
         }
 
-        if (t[i].type == TOK_KEYWORD &&
+        if (t[i].type==TOK_KEYWORD &&
             strcmp(t[i].lexeme,"void")==0 &&
             t[i+1].type ==TOK_IDENTIFIER &&
-            strcmp(t[i+2].lexeme,"(")==0) {
+            strcmp(t[i+2].lexeme,"(")==0){
 
             printf("  Method: %s (line %d)\n",
                    t[i+1].lexeme, t[i].line);
-            if (methodCount<100) {
+            if(methodCount<100) {
                 strcpy(methodNames[methodCount], t[i+1].lexeme);
                 methodLines[methodCount] = t[i].line;
             }
@@ -78,17 +78,17 @@ int classCount=0, methodCount =0;
 
     printf("\nDetected Loops:\n");
 
-    for (int i = 0; i < n; i++) {
-        if (t[i].type == TOK_KEYWORD) {
-            if (strcmp(t[i].lexeme,"for")==0) {
+    for(int i=0;i< n;i++){
+        if(t[i].type ==TOK_KEYWORD) {
+            if(strcmp(t[i].lexeme,"for")==0){
                 printf("  for-loop (line %d)\n", t[i].line);
                 forCount++;
             }
-            if (strcmp(t[i].lexeme,"while")==0) {
+            if(strcmp(t[i].lexeme,"while")==0){
                 printf("  while-loop (line %d)\n", t[i].line);
                 whileCount++;
             }
-            if (strcmp(t[i].lexeme,"do")==0) {
+            if(strcmp(t[i].lexeme,"do")==0){
                 printf("  do-while-loop (line %d)\n", t[i].line);
                 doCount++;
             }
@@ -97,12 +97,12 @@ int classCount=0, methodCount =0;
 
      printf("\nDetected Variables:\n");
 
-    int variableCount = 0;
+    int variableCount=0;
 
-    for (int i = 0; i < n - 1; i++) {
+    for(int i= 0; i<n -1;i++){
 
-    if (t[i].type == TOK_KEYWORD && is_type_keyword(t[i].lexeme) &&
-        t[i+1].type == TOK_IDENTIFIER) {
+    if(t[i].type ==TOK_KEYWORD && is_type_keyword(t[i].lexeme) &&
+        t[i+1].type== TOK_IDENTIFIER){
 
         printf("  Variable: %s (line %d)\n",
                t[i+1].lexeme, t[i].line);
@@ -110,12 +110,12 @@ int classCount=0, methodCount =0;
         variableCount++;
     }
 }
-    int lastLine= -1;                 
+    int lastLine=-1;                 
     for (int i = 0; i <n; i++) {
-        if (t[i].line !=lastLine &&
-            t[i].type !=TOK_EOF) {
+        if (t[i].line!=lastLine &&
+            t[i].type!=TOK_EOF) {
             LOC++;
-            lastLine = t[i].line;
+            lastLine=t[i].line;
         }
     }
 
@@ -129,8 +129,8 @@ int classCount=0, methodCount =0;
     printf("  Local Variables    : %d\n", localVar);
     printf("  Lines of Code (LOC)   : %d\n", LOC);
 
-    run_style_checks(t, n);
-    run_bug_checks(t, n);
+    run_style_checks(t,n);
+    run_bug_checks(t,n);
 
     char choice;
     printf("\nDo you want to see summary? (y/n): ");
@@ -145,20 +145,20 @@ int classCount=0, methodCount =0;
         int opt;
         scanf("%d", &opt);
 
-        if (opt == 1 && methodCount > 0) {
+        if (opt == 1 && methodCount>0) {
 
             printf("\nAvailable Methods:\n");
 
             int printableMethods = (methodCount < 100) ? methodCount : 100;
 
-            for (int i = 0; i < printableMethods; i++)
-                printf("  %d. %s\n", i+1, methodNames[i]);
+            for (int i = 0; i<printableMethods;i++)
+                printf("  %d. %s\n",i+1,methodNames[i]);
 
             int sel;
             printf("Enter method number: ");
             scanf("%d", &sel);
 
-            if (sel >= 1 && sel <= printableMethods) {
+            if(sel >= 1&& sel<=printableMethods){
                 char buffer[256];
                 method_name_summary(methodNames[sel-1], buffer, sizeof(buffer));
 
@@ -168,83 +168,83 @@ int classCount=0, methodCount =0;
                 printf("  Purpose : %s\n", buffer);
             }
 }
- else if (opt == 2 && classCount > 0) {
+ else if(opt==2&&classCount > 0){
 
             printf("\nAvailable Classes:\n");
 
-            int printableClasses = (classCount < 100) ? classCount : 100;
+            int printableClasses=(classCount < 100) ? classCount : 100;
 
-            for (int i = 0; i < printableClasses; i++)
+            for (int i=0; i<printableClasses;i++){
                 printf("  %d. %s\n", i+1, classNames[i]);
-
+            }
             int sel;
             printf("Enter class number: ");
             scanf("%d", &sel);
 
-            if (sel >= 1 && sel <= printableClasses) {
+            if(sel >=1&&sel<=printableClasses){
                 char buffer[256];
-                class_name_summary(classNames[sel-1], buffer, sizeof(buffer));
+                class_name_summary(classNames[sel-1], buffer,sizeof(buffer));
 
                 printf("\nClass Summary:\n");
-                printf("  Name    : %s\n", classNames[sel-1]);
-                printf("  Line    : %d\n", classLines[sel-1]);
-                printf("  Purpose : %s\n", buffer);
+                printf("  Name    : %s\n",classNames[sel-1]);
+                printf("  Line    : %d\n",classLines[sel-1]);
+                printf("  Purpose : %s\n",buffer);
             }
         }
     }
 }
 
 
-int main() {
+int main(){
  
     char dirPath[260];
     printf("Enter directory path: ");
-    fgets(dirPath, sizeof(dirPath), stdin);
+    fgets(dirPath, sizeof(dirPath),stdin);
 
-    size_t L = strlen(dirPath);
-    if (L > 0 && (dirPath[L-1] == '\n' || dirPath[L-1] == '\r')) dirPath[L-1] = '\0';
+    size_t L =strlen(dirPath);
+    if (L>0&&(dirPath[L-1]=='\n' || dirPath[L-1] == '\r')) dirPath[L-1] = '\0';
     L = strlen(dirPath);
-    if (L > 0 && dirPath[L-1] == '\r') dirPath[L-1] = '\0';
+    if (L >0 && dirPath[L-1] ='\r') dirPath[L-1] = '\0';
 
     char pattern[300];
     snprintf(pattern, sizeof(pattern), "%s\\*.java", dirPath);
 
      WIN32_FIND_DATAA ffd;
-    HANDLE hFind = FindFirstFileA(pattern, &ffd);
+    HANDLE hFind=FindFirstFileA(pattern, &ffd);
 
-    if (hFind == INVALID_HANDLE_VALUE) {
+    if(hFind==INVALID_HANDLE_VALUE){
         printf("No .java files found (or directory invalid).\n");
         return 1;
     }
 
     char files[200][260];
-    int fileCount = 0;
+    int fileCount=0;
 
     printf("\nAvailable .java files:\n");
 
-    do {
-        if (!(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-            if (fileCount < 200) {
+    do{
+        if(!(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+            if(fileCount<200){
                 strcpy(files[fileCount], ffd.cFileName);
-                printf("  %d. %s\n", fileCount + 1, files[fileCount]);
+                printf("  %d. %s\n", fileCount+1,files[fileCount]);
                 fileCount++;
             }
         }
     }
-    while (FindNextFileA(hFind, &ffd) != 0);
+    while(FindNextFileA(hFind, &ffd) !=0);
 
     FindClose(hFind);
     
-     if (fileCount == 0) {
+     if(fileCount==0){
         printf("No .java files found.\n");
         return 1;
     }
     
-    int choice = 0;
+    int choice=0;
     printf("\nSelect file number: ");
     scanf("%d", &choice);
 
-    if (choice < 1 || choice > fileCount) {
+    if(choice<1 || choice>fileCount){
         printf("Invalid selection.\n");
         return 1;
     }
@@ -253,10 +253,10 @@ int main() {
     snprintf(fullPath, sizeof(fullPath), "%s\\%s", dirPath, files[choice - 1]);
 
     int count=0;
-    Token *tokens = lex_file(fullPath, &count);
+    Token *tokens=lex_file(fullPath, &count);
     
-    if (!tokens) {
-        fprintf(stderr, "Tokenization failed\n");
+    if(!tokens){
+        fprintf(stderr,"Tokenization failed\n");
         return 1;
     }
 
